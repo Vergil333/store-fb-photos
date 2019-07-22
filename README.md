1ec3fc9d563f9cd9c6ba96465bc250be1f401036
vergil333 <Vergil333@gmail.com>
2019-07-22 09:45:17 +0200

    Login with FB JavaScript SDK

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # store-fb-photos
 Demo for RH Easy
+
+Task can be found in Wiki page of this repository.
+
+Spring boot Security:
+user: user
+password: password
+
+Spring boot
+Thymeleaf
+Lombok
+FB JavaScript SDK
--- demo/src/main/java/com/machava/demo/controllers/Web.java
+++ demo/src/main/java/com/machava/demo/controllers/Web.java
@@ -1,14 +1,17 @@
 package com.machava.demo.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class Web {
 
-    @GetMapping("/")
-    public String home() {
-        return "Hello World!";
+    @GetMapping("/photos")
+    public String showClientPhotos(@RequestParam(name="fbToken", required = true) String fbToken, Model model) {
+        model.addAttribute("fbToken", fbToken);
+        return "photos";
     }
 
 }
