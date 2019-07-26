b1ca243eb0e01eb99add685dd3a39a123e6d977e
vergil333 <Vergil333@gmail.com>
2019-07-26 20:00:44 +0200

    get users info

--- demo/src/main/java/com/machava/demo/controllers/FbApi.java
+++ demo/src/main/java/com/machava/demo/controllers/FbApi.java
@@ -1,8 +1,10 @@
 package com.machava.demo.controllers;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +13,10 @@ import org.json.JSONObject;
 import org.junit.Test;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.machava.demo.dtos.UserDto;
+
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
@@ -18,17 +24,17 @@ import kong.unirest.UnirestException;
 
 @Service
 public class FbApi {
+    String invalidToken = "EAATHPQNJw4sBAEmwEds3NNiOA8vkBLH3dx0ldeS9nUeu879b3Gw9xsSHUIBXuZCNstmVGPQZA6ORoZCWjIZCRbJ06XuZC7vCAZBWdHPxDTsOxLn5afaoS1c1yfDYU4QOOruDax3ZBrQeNDZBJ84ITvD6SHE5ZCybSuY4aOyaZCs0uP81isK3Iwe8PJLd1yZC3CbDZAoZD";
+    String validToken = "EAATHPQNJw4sBAI018NoKnpVAH0onaHFiqdw83FFVt9kzb7sOminLLrYgS41nYfJf09xIMVbQ4ofb3TiHR9jYM4FCOdDvKf6oYOGcJpdCZA5H91cGYPd48qZAZC1M6CEVciA1TAwypnsk1lhZB922yLHBVCUiFN0yu5Ods0TlQ8c6sqkNgB24DNPskXt5YEoZD";
+
 
     private static String apiUrl = "https://graph.facebook.com/v3.3/";
 
     // PhotoDto me/photos?fields=id,name,picture,link
-    // UserDto me
+    // UserDto me?fields=id,name
     // Debug FbToken debug_token?input_token={{FbToken}}&access_token=1344964768940939|_c_00YpU-mhtlroMpAdN2ftV_w8
 
     static Boolean verifyToken(String fbToken) {
-        if (fbToken == null || fbToken.equals("")) {
-            throw new IllegalArgumentException("Token cannot be null!");
-        }
 
         List<String> requiredPermissionsList = List.of("public_profile","user_photos");
 
@@ -50,8 +56,6 @@ public class FbApi {
         JSONArray scopesJsonArray = responseObject.getJSONArray("scopes");
 
         List<String> scopesList = new ArrayList<>();
-        //scopesJsonArray.toList().forEach(item -> scopesList.add(item.toString()));
-
         for (int i = 0; i < scopesJsonArray.length(); i++) {
             scopesList.add(scopesJsonArray.getString(i));
         }
@@ -73,12 +77,41 @@ public class FbApi {
     }
 
     @Test
-    public void testPermissions() {
-        String invalidToken = "EAATHPQNJw4sBAEmwEds3NNiOA8vkBLH3dx0ldeS9nUeu879b3Gw9xsSHUIBXuZCNstmVGPQZA6ORoZCWjIZCRbJ06XuZC7vCAZBWdHPxDTsOxLn5afaoS1c1yfDYU4QOOruDax3ZBrQeNDZBJ84ITvD6SHE5ZCybSuY4aOyaZCs0uP81isK3Iwe8PJLd1yZC3CbDZAoZD";
-        String validToken = "EAATHPQNJw4sBAAI4dOAiuYbzq70Oe0ZAljvHrmUCZC5bKokGesTiMgCvdjwaDpSxqafAKmFT5iP2sSgKXMu0gJ0R0mjMZALE2plewuqwV3wEiWT1gBrJlkZAGZAZBIZAJ7HrkCq8VddxEagPZAqMg5gpfu93gaFAwiauMzhmZC6uShCSDIV3RWZCtduVMxPzrGZCLAZD";
+    public void testToken() {
+        assertFalse(verifyToken(invalidToken));
+        assertTrue(verifyToken(validToken));
+    }
 
-        assertFalse(this.verifyToken(invalidToken));
-        assertTrue(this.verifyToken(validToken));
+    static UserDto getUserInfo(String fbToken) throws IOException {
+
+        HttpResponse<JsonNode> resposne = null;
+
+        try {
+            resposne = Unirest.get(apiUrl + "me")
+                    .queryString("fields", "id,name")
+                    .queryString("access_token", fbToken)
+                    .asJson();
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+
+        assert resposne != null;
+        JSONObject responseObject = resposne.getBody().getObject();
+
+        if (responseObject.has("error")) {
+            System.out.println(responseObject.getJSONObject("error").getString("message"));
+            return null;
+        } else {
+            ObjectMapper mapper = new ObjectMapper();
+            UserDto userDto = mapper.readValue(String.valueOf(responseObject), new TypeReference<UserDto>() {});
+            return userDto;
+        }
+    }
+
+    @Test
+    public void testUserInfo() throws IOException {
+        assertNotNull(getUserInfo(validToken).getId());
+        assertNotNull(getUserInfo(validToken).getName());
     }
 
 }
--- demo/src/main/java/com/machava/demo/controllers/Web.java
+++ demo/src/main/java/com/machava/demo/controllers/Web.java
@@ -1,5 +1,7 @@
 package com.machava.demo.controllers;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +11,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class Web {
 
     @GetMapping("/photos")
-    public String showClientPhotos(@RequestParam(name="fbToken", required = true) String fbToken, Model model) {
+    public String showClientPhotos(@RequestParam(name="fbToken", required = true) String fbToken, Model model) throws IOException {
+        if (fbToken == null || fbToken.equals("")) {
+            throw new IllegalArgumentException("Token cannot be null!");
+        }
+
         Boolean permissionsOk = FbApi.verifyToken(fbToken);
-        model.addAttribute("permissionsAreOk", permissionsOk);
+
+        model.addAttribute("isTokenValid", permissionsOk);
+        if (permissionsOk) {
+        }
+        model.addAttribute("userDto", FbApi.getUserInfo(fbToken));
         model.addAttribute("fbToken", fbToken);
         return "photos";
     }
